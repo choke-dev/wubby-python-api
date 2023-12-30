3827a1b2c6c5a068740dfdc3eff8781260eb382a
choke-dev <choke-dev@users.noreply.github.com>
2023-12-30 05:53:05 +0000

    ✅ Ready to clone and code.

--- wubby_python_api/app.py
+++ wubby_python_api/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+wubby_python_api API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="wubby_python_api",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://wubby_python_api.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "choke-dev",
+        "url": "http://wubby_python_api.com/contact/",
+        "email": "choke-dev@wubby_python_api.com",
     },
     license_info={
         "name": "The Unlicense",
